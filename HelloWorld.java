a4b6588eca21bbbd4daa3f708bf8ee9b4a0c6a1f
hdl12520 <1750094549@qq.com>
2020-08-05 11:59:06 +0800

    hello world

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world1!");
+	System.out.println("hello world2!");
+	System.out.println("hello world3!");
+	System.out.println("hello world4!");
+	System.out.println("hello world5!");
+    }
+}
